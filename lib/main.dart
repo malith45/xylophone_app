9750db3ce3943971a5516f59aa7c674417d0c619
Malith Kumaranayaka <127102703+malith45@users.noreply.github.com>
2024-09-23 19:52:15 +0530

    Working with function to code reduction.But every button is of same color.

--- lib/main.dart
+++ lib/main.dart
@@ -23,63 +23,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: TextButton(
-                    onPressed: () async {
-                      audioPlay(2);
-                    },
-                    child: Container(
-                      color: const Color(0xffD81B60),
-                    )),
-              ),
-              Expanded(
-                child: TextButton(
-                    onPressed: () async {
-                      audioPlay(3);
-                    },
-                    child: Container(
-                      color: const Color(0xff8E24AA),
-                    )),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    audioPlay(4);
-                  },
-                  child: Container(
-                    color: const Color(0xff5E35B1),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                    onPressed: () async {
-                      audioPlay(5);
-                    },
-                    child: Container(
-                      color: const Color(0xff3949AB),
-                    )),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    audioPlay(6);
-                  },
-                  child: Container(
-                    color: const Color(0xff1E88E5),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    audioPlay(7);
-                  },
-                  child: Container(
-                    color: const Color(0xff43A047),
-                  ),
-                ),
-              ),
+              keyBoard(1),
+              keyBoard(2),
+              keyBoard(3),
+              keyBoard(4),
+              keyBoard(5),
+              keyBoard(6),
+              keyBoard(7),
             ],
           ),
         ),
@@ -90,17 +40,17 @@ class _XylophoneAppState extends State<XylophoneApp> {
   Future<void> audioPlay(int track) async {
     final player = AudioPlayer();
     await player.play(AssetSource('audio/note$track.wav'));
+  }
 
-    void keyBoard() {
-      Expanded(
-        child: TextButton(
-            onPressed: () async {
-              audioPlay(1);
-            },
-            child: Container(
-              color: const Color(0xffE53935),
-            )),
-      );
-    }
+  Expanded keyBoard(int key) {
+    return Expanded(
+      child: TextButton(
+          onPressed: () async {
+            audioPlay(key);
+          },
+          child: Container(
+            color: const Color(0xffE53935),
+          )),
+    );
   }
 }
